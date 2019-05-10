a215871105d0afa8fea195fd95dbf35cf42096c4
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 16:41:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,17 +3,19 @@ require 'pry'
 
 def dictionary
   sub = {
-   "hello" => "hi",
-   "to" => "2",
-   "two" => "2",
-   "too" => "2",
-   "for" => "4",
-   "four" => "4",
-   "be" => "b",
-   "you" => "u",
-   "at" => "@",
-   "and" => "&"
-}
+    "hello" => 'hi',
+    "to"=> "2",
+    "too"=> "2",
+    "two" => "2",
+    "for"=>"4",
+    "four" => "4", 
+    "For"=>"4",
+    "Four" => "4",
+    'be' => "b",
+    'you'=> "u",
+    "at" =>"@",
+    "and" => "&",
+    }
 end
 
 def word_substituter(tweet)
