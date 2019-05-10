1b507aafe27a635260bdfe787bb08a600f4be2d2
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:43:02 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,13 +2,13 @@
 
 def dictionary
   sub = {
-  "hello" becomes 'hi'
-  "to, two, too" become '2' 
-  "for, four" become '4'
-  'be' becomes 'b'
-  'you' becomes 'u'
-  "at" becomes "@" 
-  "and" becomes "&"    
+    "hello" => 'hi'
+    "to, two, too" =>'2' 
+    "for, four" => '4'
+    'be' => 'b'
+    'you' => 'u'
+    "at" => "@" 
+    "and" => "&"
   }
 
 def word_substituter(tweet)
