f1e64d6e4b6c85be394d1792987d6495c5e1fa04
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 13:46:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,5 +16,8 @@ def word_substituter(tweet)
   tweet.split(' ').collect do |word| 
     if dictionary.keys.include?(word)
       word = dictionary[word]
-      
+    else
+      return word
+    end
+    
 end
